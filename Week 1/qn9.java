85186a79785297aa4a535ce2806f052a71fdac58
dilasha-poudel <np02cs4a250017@bicnepal.edu.np>
2026-03-10 10:43:56 +0545

    Update qn9.java

--- Week 1/qn9.java
+++ Week 1/qn9.java
@@ -3,13 +3,14 @@ public class qn9 {
     public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter the principle: ");
-    double principle = sc.nextFloat();
+    double principal = sc.nextFloat();
     System.out.println("Enter the rate: ");
     double rate = sc.nextFloat();
-    System.out.println("Enter the principle: ");
+    System.out.println("Enter the principal: ");
     double time = sc.nextFloat();
-    double simpleInterest = (principle*rate*time)/100;
+    double simpleInterest = (principal*rate*time)/100;
     System.out.println("The interest amount is " +simpleInterest);
     sc.close();
     }
 }
+
