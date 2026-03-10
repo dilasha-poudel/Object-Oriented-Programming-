bdd85a4dba22a03078a99a91be1422174ade4729
dilasha-poudel <np02cs4a250017@bicnepal.edu.np>
2026-03-10 09:44:19 +0545

    Update qn15.java

--- Week 1/qn15.java
+++ Week 1/qn15.java
@@ -1,17 +1,21 @@
+
+import java.util.*;
 public class qn15 {
-    public static void main(String[] args) {
-
-        boolean expr1 = (5 > 3);
-        boolean expr2 = (8 > 5);
-        System.out.println("Logical AND: " + (expr1 && expr2));
-
-
-        expr1 = (5 > 3);
-        expr2 = (2 > 5);
-        System.out.println("Logical OR: " + (expr1 || expr2));
-
-
-        boolean expr3 = !(5 == 10);
-        System.out.println("Logical NOT: " + expr3);
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a first number: ");
+    int num1 = sc.nextInt();
+    System.out.println("Enter a second number: ");
+    int num2 = sc.nextInt();
+    int sum = num1+ num2;
+    int diff = num1-num2;
+    int product = num1*num2;
+    int quotient = num1/num2;
+    System.out.println("The sum of num1 and num2 is " + sum);
+    System.out.println("The difference of num1 and num2 is " + diff);
+    System.out.println("The product of num1 and num2 is " + product);
+    System.out.println("The quotient of num1 and num2 is " + quotient);
+    sc.close();
+    
     }
 }
