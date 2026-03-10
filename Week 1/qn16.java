354df9ca904b8051cc52c3fb647b3b2b385c4749
dilasha-poudel <np02cs4a250017@bicnepal.edu.np>
2026-03-10 09:47:07 +0545

    Add files via upload

---
+++ Week 1/qn16.java
@@ -0,0 +1,18 @@
+
+public class qn16 {
+    public static void main(String[] args) {
+
+        boolean expr1 = (5 > 3);
+        boolean expr2 = (8 > 5);
+        System.out.println("Logical AND: " + (expr1 && expr2));
+
+
+        expr1 = (5 > 3);
+        expr2 = (2 > 5);
+        System.out.println("Logical OR: " + (expr1 || expr2));
+
+
+        boolean expr3 = !(5 == 10);
+        System.out.println("Logical NOT: " + expr3);
+    }
+}
